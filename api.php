0a690ca9e43372d23dbc0d18f443365bc9bf3f00
Everton Brandão de Moura <evertonbrandao137@hotmail.com>
2024-02-01 19:19:14 -0300

    conversor de moedas linkado a API do banco central com atualização dinamica

--- api.php
+++ api.php
@@ -11,37 +11,28 @@
         <h1>Conversor de Moedas: Euro e Dolar</h1>
         <p>
             <?php 
-
                 $inicio = date("m-d-Y" , strtotime("-7 days"));
 
                 $fim = date("m-d-Y");
 
-                $urlDol = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial=\''. $inicio .'\'&@dataFinalCotacao=\''. $fim .'\'&$top=1&$orderby=dataHoraCotacao%20desc&$format=json&$select=cotacaoCompra,dataHoraCotacao';
+                $moeda = $_POST["moeda"];
 
-                $dadosDol = json_decode(file_get_contents($urlDol), true);
+                $url = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoMoedaPeriodo(moeda=@moeda,dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@moeda=\''. $moeda .'\'&@dataInicial=\''. $inicio .'\'&@dataFinalCotacao=\''. $fim .'\'&$top=1&$orderby=dataHoraCotacao%20desc&$format=json&$select=cotacaoCompra,dataHoraCotacao';
 
-                $cotacaoDol = $dadosDol["value"][0]["cotacaoCompra"];
+                $dados = json_decode(file_get_contents($url), true);
 
-                $urlEur = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoMoedaPeriodo(moeda=@moeda,dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@moeda=\'EUR\'&@dataInicial=\''. $inicio .'\'&@dataFinalCotacao=\''. $fim .'\'&$top=1&$orderby=dataHoraCotacao%20desc&$format=json&$select=cotacaoCompra,dataHoraCotacao';
-
-                $dadosEur = json_decode(file_get_contents($urlEur), true);
-
-                $cotacaoEur = $dadosEur["value"][0]["cotacaoCompra"];
+                $cotacao = $dados["value"][0]["cotacaoCompra"];
 
                 $real = $_POST["real"] ?? 0;
 
-                $dolar =  $real / $cotacaoDol;
-
-                $euro  =  $real / $cotacaoEur;
+                $resultado =  $real / $cotacao;
 
                 $padrao = numfmt_create("pt_BR", NumberFormatter::CURRENCY);
 
-                echo "Seus " . numfmt_format_currency($padrao, $real, "BRL") . " equivale a " . numfmt_format_currency($padrao, $dolar, "USD") .  " Dolares" . " e também equivale a " . numfmt_format_currency($padrao, $euro, "EUR") . " Euros";
-                
-
+                echo "Seus " . numfmt_format_currency($padrao, $real, "BRL") . " equivale a " . numfmt_format_currency($padrao, $resultado, $moeda);
             ?>
             </p>
-    <button onclick="javascript:window.location.href='index.html';">&#x2B05; Voltar</button>
+        <button onclick="javascript:window.location.href='index.html';">&#x2B05; Voltar</button>
     </main>
 </body>
 </html>
